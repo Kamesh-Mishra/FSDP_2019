3dc8078f8f6b941ed1a64820b3e8e7a54d1ec6f3
Kamesh-Mishra <kamesh.mishra@gmail.com>
2019-12-03 16:33:30 +0530

    added new file

---
+++ code_problem/2/circle.py
@@ -0,0 +1,12 @@
+#Area and Perimeter of Circle
+
+radius = float(input("Enter the value of radius:"))
+import math
+from math import pi
+math.pi
+
+area= (pi * radius * radius)
+
+perimeter = (2 * pi * radius)
+
+print("Area of circle is:",area,"Perimeter of circle is:",perimeter)
