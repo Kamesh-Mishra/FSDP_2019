1bfe0bf223fb938448b04c028ec03ffd84c51328
Kamesh-Mishra <kamesh.mishra@gmail.com>
2019-12-04 15:42:49 +0530

    added new file

--- code_problem/2/circle.py
+++ code_problem/2/circle.py
@@ -10,3 +10,110 @@ area= (pi * radius * radius)
 perimeter = (2 * pi * radius)
 
 print("Area of circle is:",area,"Perimeter of circle is:",perimeter)
+
+
+
+#***********************
+#***********************
+#***********************
+'''Hands On'''
+string = "Rajasthan"
+#Print characters at the even index number
+
+string = "Rajasthan"
+print(string[0::2])
+
+
+
+
+'''Hands On'''
+string = "Rajasthan"
+#Print the given string in reverse 
+
+print(string[::-1])
+
+
+
+'''Hands On'''
+string = "Forsk Technologies"
+#Print Forsk using slicing ( forward indexing Left to Right  )  
+
+print(string[0:5])
+
+
+'''Hands On'''
+string = "Forsk Technologies"
+#Print Technologies using slicing ( forward indexing Left to Right )  
+
+print(string[6:])
+
+
+
+'''Hands On'''
+string = "Forsk Technologies"
+#Print Forsk using slicing ( Reverse indexing Right to Left  )  
+
+print(string[-13:-7])
+
+
+'''Hands On'''
+string = "Forsk Technologies"
+#Print Technologies using slicing ( forward indexing Right to Left )  
+
+
+
+
+'''Hands On'''
+string = "Forsk Technologies"
+#Print ksroF using slicing ( forward indexing and Reverse  Indexing)  
+
+print(string[4::-1])
+print(string[:])
+
+
+'''Hands On'''
+string = "Forsk Technologies"
+#Print siesgolonhceT using slicing ( forward indexing Left to Right )  
+
+print(string[:5:-1])
+
+
+'''Hands On'''
+#Print Technologies Forsk using slicing ( forward indexing Left to Right  )  
+
+string = "Forsk Technologies"
+print(string[6::],string[0:5])
+
+
+'''Hands On'''
+#Print Technologies Forsk using slicing ( Reverse indexing Right to Left ) 
+
+string = "Forsk Technologies"
+
+
+'''Hands On'''
+# Given strings a and b, return a single string with a and b separated
+# by a space '<a> <b>', except swap the first 2 chars of each string.
+# e.g.
+#   'mix', pod' -> 'pox mid'
+#   'dog', 'dinner' -> 'dig donner'
+# Assume a and b are length 2 or more.
+
+'''########################################################'''
+'''########################################################'''
+'''########################################################'''
+
+
+'''Hands On''' 
+# Write a program to count the number of words in a sentence.
+
+string = input("enter the sentence:")
+
+print(len(string.split()),"number of words is in the sentence")
+
+
+'''Hands On'''
+# Develop a program that will display a sentence backwards after entered.
+
+
+
