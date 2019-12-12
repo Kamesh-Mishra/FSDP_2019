153ba6a8ada53f0b3b6ed1464c77cf4e646d4d71
Kamesh-Mishra <kamesh.mishra@gmail.com>
2019-12-12 16:44:54 +0530

    Added new file

--- code_problem/5/hands_on.py
+++ code_problem/5/hands_on.py
@@ -1,5 +1,3 @@
-
-"""
 '''Hands On'''
 # Take the list of the parts of your street address
 # Write a loop that goes through that list and prints out each item in that list
@@ -23,7 +21,7 @@ print("count of strings: ",len(words))
 print("count of strings: ",len(words1))
 print("count of strings: ",len(words2))
 
-"""
+
 
 '''Hands On'''
 # Take your street address and make it a list variable myaddress
@@ -47,4 +45,76 @@ for i in myaddress:
     if i.isnumeric():
         address_sum = address_sum+i
 print(address_sum)
+
+
+
+
+
+
+'''Hands On'''
+#Looping through a list of temperatures and applying a test
+#Pretend you have the following list of temperatures T:
+T = [273.4, 265.5, 277.7, 285.5]
+#and a list of flags called Tflags that is initialized to all False
+Tflags = [False, False, False, False]
+#Write a loop that checks each temperature in T and sets the corresponding
+#Tflags element to True if the temperature is above the freezing point of water.
+
+T = [273.4, 265.5, 277.7, 285.5]
+
+Tflags = [False, False, False, False]
+n = 0
+for i in T:
+    if T[n] > 273:
+        print("Temperature in T is:",T[n])
+        print("temperature is above the freezing point of water:", not Tflags[n])
+    else:
+        print("Temperature in T is:",T[n])
+        print("temperature is above the freezing point of water:", Tflags[n])
+        
+    n = n+1
+
+
+
+'''Hands On'''
+# Take the list of the parts of your street address
+# Write a loop that goes through that list and prints out each item in that list
+myaddress = [3225, 'West', 'Foster', 'Avenue', 'Chicago', 'IL', 60625]
+
+for i in myaddress:
+    print(i)
+
+
+'''Hands On'''
+# Create a list of Fibonnaci numbers up to the 50th term.
+# The program will then ask the user for which position in the sequence
+# they want to know the Fibonacci value for
+# The Fibonacci sequence was originally used as a basic model for rabbit population growth:
+
+a = 0
+b = 1
+
+n = int(input("number of term: "))
+
+list1 = [0,1]
+for i in range(n-2):
+    
+    c = a+b
+
+    a= b
+    b = c
+    
+    
+    list1.append(c)
+print(list1)
+  
+inp = int(input("Enter position in the sequence: "))
+print(list1[inp-1])
+   
+
+
+
+
+
+
 
