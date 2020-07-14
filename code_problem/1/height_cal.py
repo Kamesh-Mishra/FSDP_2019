add252298aad3d4b10e10b9f1bc02df1a532d75e
Kamesh-Mishra <kamesh.mishra20@gmail.com>
2020-07-14 12:18:33 +0530

    Added new file

--- code_problem/1/height_cal.py
+++
@@ -1,9 +0,0 @@
-#Height Calculator
-
-height = float(input("Enter height:"))
-
-height_in_meter = (height*0.3048)
-print("your height in meter is:",height_in_meter,"m")
-
-height_in_centimetre = (height*0.0254)
-print("your height in centimeter is:",height_in_centimetre,"cm")
