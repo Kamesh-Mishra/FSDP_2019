127d4364a86436fa4dcd277e461b6f0e094c8411
Kamesh-Mishra <kamesh.mishra20@gmail.com>
2020-07-15 11:32:47 +0530

    Added enw file

--- code_problem/1/ponderal_cal.py
+++
@@ -1,7 +0,0 @@
-#Ponderal Index Calculators
-weight=float(input("Enter your weight in kg:"))
-height=float(input("Enter your height in meter:"))
-
-BMI=((weight/height)/height)
-Ponderal_Index = BMI/height
-print("your Ponderal Index is:",Ponderal_Index)
