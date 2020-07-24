d4fdf450a7fabc5c7432de03a9c43186712a5fcd
Kamesh-Mishra <kamesh.mishra20@gmail.com>
2020-07-25 00:37:16 +0530

    Added new file

--- code_problem/2/checker.py
+++
@@ -1,9 +0,0 @@
-# Name Printing Checkerboard pattern
-print("\u2731 "*8)
-print(" \u2731"*8)
-print("\u2731 "*8)
-print(" \u2731"*8)
-print("\u2731 "*8)
-print(" \u2731"*8)
-print("\u2731 "*8)
-print(" \u2731"*8)
