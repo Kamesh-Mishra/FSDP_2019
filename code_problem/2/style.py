ad8b405e4d893c635cd867a9808f983d502ef522
Kamesh-Mishra <kamesh.mishra20@gmail.com>
2020-07-31 00:49:10 +0530

    Added new file

--- code_problem/2/style.py
+++
@@ -1,7 +0,0 @@
-#Styling of String
-
-name = input("Enter name:")
-
-print("name in uppercase character is:",name.upper())
-print("name in lowercase character is:",name.lower())
-print("name in TitleCase character is:",name.title())
